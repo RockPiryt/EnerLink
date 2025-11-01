a229144129374cefee7267a10860fae4e9a6761c
RockPiryt <rockpiryt@gmail.com>
2025-11-01 15:44:01 +0100

    Add back populates to models

--- backend/app/models/history_model.py
+++ backend/app/models/history_model.py
@@ -7,6 +7,12 @@ class Action(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
 
+    user_changes = db.relationship("UserChangeHistory", back_populates="action")
+    user_logs = db.relationship("UserLogHistory", back_populates="action")
+    role_changes = db.relationship("RoleChangeHistory", back_populates="action")
+
+    def __repr__(self):
+        return f"<Action {self.name}>"
 
 class UserChangeHistory(db.Model):
     __tablename__ = "user_change_history"
@@ -17,6 +23,16 @@ class UserChangeHistory(db.Model):
     id_action = db.Column(db.Integer, db.ForeignKey("action.id"))
     change_time = db.Column(db.DateTime, default=datetime.utcnow)
 
+    changed_by = db.relationship(
+        "User", foreign_keys=[id_user_who_changed], back_populates="changes_made"
+    )
+    changed_user = db.relationship(
+        "User", foreign_keys=[id_user_changed], back_populates="changes_received"
+    )
+    action = db.relationship("Action", back_populates="user_changes")
+
+    def __repr__(self):
+        return f"<UserChangeHistory action={self.id_action} at={self.change_time}>"
 
 class UserLogHistory(db.Model):
     __tablename__ = "user_log_history"
@@ -26,6 +42,11 @@ class UserLogHistory(db.Model):
     id_action = db.Column(db.Integer, db.ForeignKey("action.id"))
     log_time = db.Column(db.DateTime, default=datetime.utcnow)
 
+    user = db.relationship("User", back_populates="logs")
+    action = db.relationship("Action", back_populates="user_logs")
+
+    def __repr__(self):
+        return f"<UserLogHistory user={self.id_user} action={self.id_action}>"
 
 class RoleChangeHistory(db.Model):
     __tablename__ = "role_change_history"
@@ -35,3 +56,12 @@ class RoleChangeHistory(db.Model):
     id_role = db.Column(db.Integer, db.ForeignKey("role.id"))
     id_action = db.Column(db.Integer, db.ForeignKey("action.id"))
     change_time = db.Column(db.DateTime, default=datetime.utcnow)
+
+    changed_by = db.relationship(
+        "User", foreign_keys=[id_user_who_changed], back_populates="role_changes_made"
+    )
+    role = db.relationship("Role", back_populates="role_changes")
+    action = db.relationship("Action", back_populates="role_changes")
+
+    def __repr__(self):
+        return f"<RoleChangeHistory role={self.id_role} at={self.change_time}>"
--- backend/app/models/user_model.py
+++ backend/app/models/user_model.py
@@ -8,6 +8,12 @@ class Role(db.Model):
     role_name = db.Column(db.String(30))
     active = db.Column(db.Boolean, default=True)
 
+    users = db.relationship("User", back_populates="role")
+    role_changes = db.relationship("RoleChangeHistory", back_populates="role")
+
+    def __repr__(self):
+        return f"<Role {self.role_name}>"
+
 
 class Password(db.Model):
     __tablename__ = "password"
@@ -16,6 +22,10 @@ class Password(db.Model):
     pass_hash = db.Column("pass", db.String(20))
     creation_at = db.Column(db.DateTime, default=datetime.utcnow)
 
+    user = db.relationship("User", back_populates="password", uselist=False)
+
+    def __repr__(self):
+        return f"<Password id={self.id}>"
 
 class User(db.Model):
     __tablename__ = "user"
@@ -29,5 +39,25 @@ class User(db.Model):
     active = db.Column(db.Boolean, default=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    role = db.relationship("Role")
-    password = db.relationship("Password", uselist=False)
+    role = db.relationship("Role", back_populates="users")
+    password = db.relationship("Password", back_populates="user")
+
+    changes_made = db.relationship(
+        "UserChangeHistory",
+        foreign_keys="[UserChangeHistory.id_user_who_changed]",
+        back_populates="changed_by",
+    )
+    changes_received = db.relationship(
+        "UserChangeHistory",
+        foreign_keys="[UserChangeHistory.id_user_changed]",
+        back_populates="changed_user",
+    )
+    logs = db.relationship("UserLogHistory", back_populates="user")
+    role_changes_made = db.relationship(
+        "RoleChangeHistory",
+        foreign_keys="[RoleChangeHistory.id_user_who_changed]",
+        back_populates="changed_by",
+    )
+
+    def __repr__(self):
+        return f"<User {self.first_name} {self.last_name}>"
