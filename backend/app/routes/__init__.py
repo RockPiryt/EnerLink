bfea8b41daf0c17ec8187030e7dc4032bf184709
RockPiryt <rockpiryt@gmail.com>
2025-12-23 12:42:40 +0100

    Updat url_prefix

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -9,12 +9,12 @@ from .sales_routes import sale_bp
 from .tag_routes import tag_bp
 
 def register_routes(app):
-    app.register_blueprint(auth_bp, url_prefix="/api/auth")
-    app.register_blueprint(user_bp, url_prefix="/api/users")
-    app.register_blueprint(customer_bp, url_prefix="/api/customers/")
-    app.register_blueprint(dictionary_bp, url_prefix="/api/dictionary")
-    app.register_blueprint(manager_bp, url_prefix="/api/manager")
-    app.register_blueprint(provider_bp, url_prefix="/api/providers")
-    app.register_blueprint(role_bp, url_prefix="/api/roles")
-    app.register_blueprint(sale_bp, url_prefix="/api/sales")
-    app.register_blueprint(tag_bp, url_prefix="/api/tags")
+    app.register_blueprint(auth_bp, url_prefix="/api")
+    app.register_blueprint(user_bp, url_prefix="/api")
+    app.register_blueprint(customer_bp, url_prefix="/api")
+    app.register_blueprint(dictionary_bp, url_prefix="/api")
+    app.register_blueprint(manager_bp, url_prefix="/api")
+    app.register_blueprint(provider_bp, url_prefix="/api")
+    app.register_blueprint(role_bp, url_prefix="/api")
+    app.register_blueprint(sale_bp, url_prefix="/api")
+    app.register_blueprint(tag_bp, url_prefix="/api")
