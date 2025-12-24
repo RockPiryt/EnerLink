3d78ecc027dd1fe3a584a46843f501048153f214
RockPiryt <rockpiryt@gmail.com>
2025-12-24 08:09:10 +0100

    Update init for models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,2 +1,11 @@
-from .user_model import User, Role, Password
+from .address_model import Address, Country, City, District
+from .address_relation_model import CustomerAddress, SupplierAddress
+from .contract_model import Contract, ContractTimeline
+from .customer_model import Customer
 from .history_model import Action, UserChangeHistory, UserLogHistory, RoleChangeHistory
+from .pkwiu_model import Pkwiu
+from .ppe_model import PPE
+from .supplier_model import EnergySupplier, SupplierOffer, EnergyTariff, PowerUnit, CurrencyUnit
+from .tag_model import Tag
+from .user_model import User, Role, Password
+
