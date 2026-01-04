0af2f1df1dcd0007352dbbc74ccd926b772cc18a
Marcin Gierszewski <m.gierszewski.058@studms.ug.edu.pl>
2026-01-04 11:59:13 +0100

    Add placeholder frontend components for city, country, and province management

---
+++ frontend/src/components/dictionary/CountryList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CountryList: React.FC = () => {
+  // TODO: Fetch countries from backend and display
+  return (
+    <div>
+      <h2>Country Management</h2>
+      {/* Add/Edit/Deactivate/Activate/Import UI here */}
+      <p>Feature coming soon...</p>
+    </div>
+  );
+};
+
+export default CountryList;
