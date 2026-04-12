22df93c806b6cdfc55abbb376f1cc219bdc88013
MariuszDudzik <m.dudzik.067@studms.ug.edu.pl>
2026-04-12 11:56:21 +0200

    add method sync_localities to api_locations_routes

--- backend/app/routes/api_locations_routes.py
+++ backend/app/routes/api_locations_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from app.db import db
-from app.models.address_model import District
-from app.services.teryt_service import get_districts
+from app.models.address_model import District, City
+from app.services.teryt_service import get_districts, get_localities
 from flask_jwt_extended import jwt_required
 
 
@@ -29,6 +29,35 @@ def sync_districts():
             "message": "Synchronizacja zakończona.",
             "added": len(new_districts),
             "total": District.query.count()
+        }), 200
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+    
+
+@location_bp.route("/localities/sync", methods=["POST"])
+@jwt_required()
+def sync_localities():
+    try:
+        localities_from_teryt = get_localities()
+
+        existing_names = {c.name for c in City.query.all()}
+
+        new_cities = []
+        for item in localities_from_teryt:
+            name = item["name"]
+            if name not in existing_names:
+                new_cities.append(City(name=name))
+
+        if new_cities:
+            db.session.add_all(new_cities)
+            db.session.commit()
+
+        return jsonify({
+            "message": "Synchronizacja zakończona.",
+            "added": len(new_cities),
+            "total": City.query.count()
         }), 200
 
     except Exception as e:
