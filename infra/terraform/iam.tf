0c3c73773ab23e37e77436cd365e756e08f84da3
RockPiryt <rockpiryt@gmail.com>
2026-04-20 21:11:27 +0200

    feat(): Add IAM section

---
+++ infra/terraform/iam.tf
@@ -0,0 +1,30 @@
+# Trust policy allowing EC2 instances to use this role
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+# IAM role for the application EC2 instance
+resource "aws_iam_role" "ec2_node_role" {
+  name               = "preg-ec2-node-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+# Allow EC2 instance to connect through AWS Systems Manager
+resource "aws_iam_role_policy_attachment" "ec2_ssm_core" {
+  role       = aws_iam_role.ec2_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Instance profile attached to the application EC2 instance
+resource "aws_iam_instance_profile" "ec2_node_profile" {
+  name = "preg-ec2-node-profile"
+  role = aws_iam_role.ec2_node_role.name
+}
