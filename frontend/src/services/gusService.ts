e5c0a6a88be538335b5d9adab8b8a03bfcc740c1
Marcin Gierszewski <m.gierszewski.058@studms.ug.edu.pl>
2026-05-12 23:01:28 +0200

    Gus service update

---
+++ frontend/src/services/gusService.ts
@@ -0,0 +1,18 @@
+import axiosInstance from "../interceptor/interceptor";
+
+export interface GusCompanyData {
+  name: string;
+  nip: string;
+  regon: string;
+  street: string;
+  building: string;
+  local: string;
+  postcode: string;
+  city: string;
+  source: string;
+}
+
+export async function lookupNip(nip: string): Promise<GusCompanyData> {
+  const response = await axiosInstance.get(`/api/lookup/nip/${nip}`);
+  return response.data;
+}
